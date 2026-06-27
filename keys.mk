LineageOS/scripts

lineage-priv-template: Add AVB_CUSTOM_*, WITH_AVB support
See: I0578219addb01f094ecd9ec386e820aaf972c284
Change-Id: I0c7f20a49e7f6db171c67e92fca61b7ce89eea4f

lineage-priv-template/keys.mk
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
+AVB_CUSTOM_KEY_PATH := vendor/lineage-priv/keys/avb.pk8
+AVB_CUSTOM_ALGORITHM := SHA256_RSA4096
+WITH_AVB ?= false
+
 PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.adbd:com.android.adbd.certificate.override \
     com.android.adservices:com.android.adservices.certificate.override \

Meow-prjkt/android_vendor_lineage-priv_keys

keys.mk
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
+AVB_CUSTOM_KEY_PATH := vendor/evolution-priv/keys/avb.pk8
+AVB_CUSTOM_ALGORITHM := SHA256_RSA4096
+WITH_AVB ?= false
+
 PRODUCT_CERTIFICATE_OVERRIDES := \
     com.android.adbd:com.android.adbd.certificate.override \
     com.android.adservices:com.android.adservices.certificate.override \